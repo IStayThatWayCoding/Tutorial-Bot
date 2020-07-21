c1771220799f56d4615f9b2ea84d87750da98afe
IStayThatWayCoding <68560774+IStayThatWayCoding@users.noreply.github.com>
2020-07-21 17:13:45 -0400

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,24 @@ const bot = new Discord.Client();
 
 bot.on('ready', async () => {
     console.log("Online")
+    bot.user.setActivity("IStayThatWay on YouTube", {type: "WATCHING"});
 });
+
+bot.on("message", async message => {
+    if(message.author.bot || message.channel.type === 'dm') return;
+
+    let prefix = config.prefix;
+    let messageArray = message.content.split(" ");
+    let cmd = messageArray[0];
+    let args = messageArray.slice(1);
+
+    if(cmd === `${prefix}test`){
+        return message.channel.send("Testing 1234!")
+    }
+
+    if(cmd === `${prefix}hello`){
+        return message.reply("Hi!")
+    }
+})
 
 bot.login(config.token);    
